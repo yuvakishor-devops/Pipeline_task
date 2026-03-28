bc82c7b1f9ad30cf9001ffac758bb29815eb27a5
yuvakishor-devops <yuvakishor.b2000@gmail.com>
2026-03-28 12:21:06 +0000

    Final DevOps project setup with ESLint, Dockerignore, and clean repo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files (index.html, css, js)
+// Serve static files
 app.use(express.static(__dirname));
 
 // Route for homepage
